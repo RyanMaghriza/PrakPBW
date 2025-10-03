19bc4f83e023686b3b53878fdc4eee20779d0a4e
RyanMaghriza <4519210088@univpancasila.ac.id>
2025-10-03 11:16:32 +0700

    Praktikum

---
+++ LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Informasi Kontak</title>
+</head>
+<body>
+    <h1>Informasi Kontak</h1>
+
+  <p>Email: ryan@email.com</p>
+  <p>Telepon: 0812-3456-7890</p>
+    <a href="/">Kembali ke Halaman Utama</a>
+</body>
+</html>
