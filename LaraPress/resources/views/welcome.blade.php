662a8109de6df0f1003598bf03076ca362146357
RyanMaghriza <4519210088@univpancasila.ac.id>
2025-10-03 11:32:08 +0700

    Pertemuan 3

---
+++ LaraPress/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang di LaraPress</title>
+</head>
+<body>
+    <h1>Selamat Datang di Blog LaraPress</h1>
+    <p>Ini adalah halaman utama dari aplikasi blog kita.</p>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a><br>
+    <a href="/kontak">Lihat Informasi Kontak</a>
+
+
+</body>
+</html>
+
